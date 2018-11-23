6e0ca6fb1b05ceb43deea14f96214db84416b574
Kamil Szpakowski <32940567+shizzeer@users.noreply.github.com>
2018-11-24 00:56:12 +0100

    Added create_menu function

--- project/hackers_eye.c
+++ project/hackers_eye.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "../gui/container.h"
+#include "../gui/menu.h"
 // #include <gdk/gdkkeysyms.h>
 
 int main(int argc, char* argv[])
@@ -14,10 +15,14 @@ int main(int argc, char* argv[])
 	icon = create_icon("eye.jpg");
 	gtk_window_set_icon(GTK_WINDOW(window), icon);
 
-	gtk_widget_show_all(window);
+	vbox = gtk_vbox_new(FALSE, 0);
+	create_menu(vbox);
+	gtk_container_add(GTK_CONTAINER(window), vbox);
+
 	set_destroy_signal(window);
-	
 	free_icon_object(icon);
+	
+	gtk_widget_show_all(window);
 
 	gtk_main();
 	
