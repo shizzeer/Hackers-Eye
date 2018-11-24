f98367b01268f8fd91806b1686c228b7a7f2783e
Kamil Szpakowski <32940567+shizzeer@users.noreply.github.com>
2018-11-24 21:05:33 +0100

    Added icons to the menu items

--- gui/menu.c
+++ gui/menu.c
@@ -10,32 +10,62 @@ static void add_separator_to_menu(GtkWidget *menu)
 
 static void add_items_to_file_menu(GtkWidget *menu)
 {
-	const char *file_menu_items[7] = {"New", "Open", "Save", "Save as", "Export", "Close", "Exit"};
 	GtkWidget *file_menu_item;
 
 	for (int i = 0; i < 7; i++)
 	{
-		file_menu_item = gtk_menu_item_new_with_label(file_menu_items[i]);
-
-		if (strcmp(file_menu_items[i], "Export") == 0 || strcmp(file_menu_items[i], "Close") == 0)
-			add_separator_to_menu(menu);
-		
+		switch (i)
+		{
+			case 0: file_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW, NULL); break;
+			case 1: file_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_OPEN, NULL); break;
+			case 2: file_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_SAVE, NULL); break;
+			case 3: file_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_SAVE_AS, NULL); break;
+			case 4: 
+			{
+				add_separator_to_menu(menu);
+				file_menu_item = gtk_menu_item_new_with_label("Export"); 
+			}
+			break;
+			case 5:
+			{
+				add_separator_to_menu(menu);
+				file_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_CLOSE, NULL);
+			}
+			break;
+			case 6: file_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_QUIT, NULL); break;
+			default: fprintf(stderr, "%s\n", "No such menu item!"); break;
+		}
 		gtk_menu_shell_append(GTK_MENU_SHELL(menu), file_menu_item);
 	}
 }
 
 static void add_items_to_edit_menu(GtkWidget *menu)
 {
-	const char *edit_menu_items[7] = {"Undo", "Redo", "Cut", "Copy", "Paste", "Delete", "Select All"};
 	GtkWidget *edit_menu_item;
 
 	for (int i = 0; i < 7; i++)
 	{
-		edit_menu_item = gtk_menu_item_new_with_label(edit_menu_items[i]);
-
-		if (strcmp(edit_menu_items[i], "Cut") == 0 || strcmp(edit_menu_items[i], "Select All") == 0)
-			add_separator_to_menu(menu);
-
+		switch (i)
+		{
+			case 0: edit_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_UNDO, NULL); break;
+			case 1: edit_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_REDO, NULL); break;
+			case 2:
+			{
+				add_separator_to_menu(menu);
+				edit_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_CUT, NULL);
+			}
+			break;
+			case 3: edit_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_COPY, NULL); break;
+			case 4: edit_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_PASTE, NULL); break;
+			case 5: edit_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_DELETE, NULL); break;
+			case 6:
+			{
+				add_separator_to_menu(menu);
+				edit_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_SELECT_ALL, NULL);
+			}
+			break;
+			default: fprintf(stderr, "%s\n", "No such menu item!"); break;
+		}
 		gtk_menu_shell_append(GTK_MENU_SHELL(menu), edit_menu_item);
 	}
 }
@@ -47,23 +77,36 @@ static void add_items_to_view_menu(GtkWidget *menu)
 
 	for (int i = 0; i < 2; i++)
 	{
-		view_menu_item = gtk_menu_item_new_with_label(view_menu_items[i]);
+		switch (i)
+		{
+			case 0: view_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_SELECT_FONT, NULL); break;
+			case 1: view_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_SELECT_COLOR, NULL); break;
+			default: fprintf(stderr, "%s\n", "No such menu item!"); break;
+		}
 		gtk_menu_shell_append(GTK_MENU_SHELL(menu), view_menu_item);
 	}
 }
 
 static void add_items_to_search_menu(GtkWidget *menu)
 {
-	const char *search_menu_items[4] = {"Search", "Search next", "Search previous", "Goto offset"};
 	GtkWidget *search_menu_item;
 
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 5; i++)
 	{
-		search_menu_item = gtk_menu_item_new_with_label(search_menu_items[i]);
-
-		if (strcmp(search_menu_items[i], "Goto offset") == 0)
-			add_separator_to_menu(menu);
-
+		switch (i)
+		{
+			case 0: search_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_FIND, NULL); break;
+			case 1: search_menu_item = gtk_menu_item_new_with_label("Find next"); break;
+			case 2: search_menu_item = gtk_menu_item_new_with_label("Find previous"); break;
+			case 3: search_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_FIND_AND_REPLACE, NULL); break;
+			case 4: 
+			{
+				add_separator_to_menu(menu);
+				search_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_JUMP_TO, NULL); 
+			}
+			break;
+			default: fprintf(stderr, "%s\n", "No such menu item!"); break;
+		}
 		gtk_menu_shell_append(GTK_MENU_SHELL(menu), search_menu_item);
 	}
 }
@@ -77,21 +120,24 @@ static void add_items_to_tools_menu(GtkWidget *menu)
 	{
 		tools_menu_item = gtk_menu_item_new_with_label(tools_menu_items[i]);
 
-		if (strcmp(tools_menu_items[i], "Bitwise operations") == 0)
+		if (i == 1)
 			add_separator_to_menu(menu);
-
+		
 		gtk_menu_shell_append(GTK_MENU_SHELL(menu), tools_menu_item);
 	}
 }
 
 static void add_items_to_help_menu(GtkWidget *menu)
 {
-	const char *help_menu_items[2] = {"Documentation", "About"};
 	GtkWidget *help_menu_item;
 
 	for (int i = 0; i < 2; i++)
 	{
-		help_menu_item = gtk_menu_item_new_with_label(help_menu_items[i]);
+		switch (i)
+		{
+			case 0: help_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_HELP, NULL); break;
+			case 1: help_menu_item = gtk_image_menu_item_new_from_stock(GTK_STOCK_ABOUT, NULL); break;
+		}
 		gtk_menu_shell_append(GTK_MENU_SHELL(menu), help_menu_item);
 	}
 }
