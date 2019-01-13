966e1451ce647973041fa5ca1496de22581886d3
Kamil Szpakowski <32940567+shizzeer@users.noreply.github.com>
2019-01-13 13:39:13 +0100

    Added menu options to the GUI window

--- project/hackers_eye.c
+++ project/hackers_eye.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include "../gui/container.h"
 #include "../gui/menu.h"
-// #include <gdk/gdkkeysyms.h>
+
+void create_gui(GtkWidget *window)
+{
+	window = create_window();
+	create_icon("eye.jpg", window);
+	create_menu(window);
+
+	gtk_widget_show_all(window);
+}
 
 int main(int argc, char* argv[])
 {
-	GtkWidget *window, *vbox;
-	GdkPixbuf *icon;
+	GtkWidget *window;
 
 	gtk_init(&argc, &argv);
 	
-	window = create_window();
-
-	icon = create_icon("eye.jpg");
-	gtk_window_set_icon(GTK_WINDOW(window), icon);
-
-	vbox = gtk_vbox_new(FALSE, 0);
-	create_menu(vbox);
-	gtk_container_add(GTK_CONTAINER(window), vbox);
-
-	set_destroy_signal(window);
-	free_icon_object(icon);
+	create_gui(window);
 	
-	gtk_widget_show_all(window);
-
 	gtk_main();
 	
 	return 0;
