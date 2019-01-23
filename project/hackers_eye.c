17cb70f407552ca5d8a1b5f66ab635f97790ac6c
Kamil Szpakowski <32940567+shizzeer@users.noreply.github.com>
2019-01-23 23:25:14 +0100

    hackers_eye: added toolbar and main vbox

--- project/hackers_eye.c
+++ project/hackers_eye.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
-#include "../gui/container.h"
-#include "../gui/menu.h"
+#include "container.h"
+#include "menu.h"
+#include "toolbar.h"
 
-void create_gui(GtkWidget *window)
+void create_gui(GtkWidget *window, GtkWidget *main_vbox)
 {
 	window = create_window();
+	main_vbox = create_main_vbox(window);
+
 	create_icon("eye.jpg", window);
-	create_menu(window);
+	create_menu(window, main_vbox);
+	create_toolbar(window, main_vbox);
 
 	gtk_widget_show_all(window);
 }
 
 int main(int argc, char* argv[])
 {
-	GtkWidget *window;
+	GtkWidget *window, *main_vbox;
 
 	gtk_init(&argc, &argv);
 	
-	create_gui(window);
+	create_gui(window, main_vbox);
 	
 	gtk_main();
 	
